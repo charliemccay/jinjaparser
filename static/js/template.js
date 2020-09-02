f8ce46ea9082dd66ad3d0d5aaa58bd81d35cf14e
Yura Bobkov <yura_@tut.by>
2020-09-02 23:12:36 +0300

    fix js files

--- static/js/template.js
+++ static/js/template.js
@@ -2,6 +2,16 @@ $(document).ready(function () {
   var bc = new BroadcastChannel('parser_channel');
   const element = $('#template');
 
+  const openWindow = (str) => {
+    var windowObjectReference = null;
+
+    if (windowObjectReference == null || windowObjectReference.closed) {
+      window.open(str, '', 'resizable,scrollbars,status');
+    } else {
+      windowObjectReference.focus();
+    }
+  };
+
   element.keyup(() => {
     bc.postMessage({ template: element.val() });
   });
--- static/js/values.js
+++ static/js/values.js
@@ -2,6 +2,16 @@ $(document).ready(function () {
   var bc = new BroadcastChannel('parser_channel');
   const element = $('#values');
 
+  const openWindow = (str) => {
+    var windowObjectReference = null;
+
+    if (windowObjectReference == null || windowObjectReference.closed) {
+      window.open(str, '', 'resizable,scrollbars,status');
+    } else {
+      windowObjectReference.focus();
+    }
+  };
+
   element.keyup(() => {
     bc.postMessage({ values: element.val() });
   });
